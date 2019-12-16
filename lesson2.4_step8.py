449095c121b3edca3bdd2529817dae8a5ddb3c1f
AVN <nachigin88@mail.ru>
2019-12-16 16:17:47 +0800

    добавил файл с прошлого урока

---
+++ lesson2.4_step8.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    # говорим WebDriver искать каждый элемент в течение 5 секунд
+    #browser.implicitly_wait(5)
+    #browser.get("http://suninjuly.github.io/cats.html")
+    #browser.find_element_by_id("button")
+
+    # говорим WebDriver ждать все элементы в течение 5 секунд
+    #browser.implicitly_wait(5)
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    button = browser.find_element_by_id("book")
+    button.click()
+
+    x1 = browser.find_element_by_id("input_value").text
+    y = calc(x1)
+    print("value x: ", x1, " value y: ", y)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
